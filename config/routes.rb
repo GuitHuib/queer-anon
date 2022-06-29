66cbc0c47eac279b9b28e30f14ba9d0adacbd58b
= <=>
2022-06-29 14:33:53 -0700

    add home page and test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "application#hello"
+  root "static_pages#home"
 end
